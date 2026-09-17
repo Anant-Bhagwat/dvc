fd5dfe727694f899b566b507ba7dd51f3d29c990
Abhishek Kumar <abhishekkumar08528@gmail.com>
2026-09-17 16:19:30 +0530

    Added Codes

--- src/services/holder-keys.js
+++ src/services/holder-keys.js
@@ -1,18 +1,6 @@
 import crypto from "node:crypto";
 
-/**
- * Generate an EC P-256 key pair for the credential holder.
- *
- * Private key:
- * - Stays with the holder
- * - Will later be used to sign the KB-JWT
- * - Must NOT be sent to the issuer API
- *
- * Public key:
- * - Can be shared with the issuer
- * - Will later be converted to JWK
- * - Will be stored in the credential as cnf.jwk
- */
+
 export function generateHolderKeyPair() {
   const { publicKey, privateKey } = crypto.generateKeyPairSync("ec", {
     namedCurve: "prime256v1",
